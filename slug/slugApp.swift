2a8d854daee3578192aa5b9418948f4e6ab0c826
winner14 <winnerkpodo0@gmail.com>
2026-03-18 19:19:58 +0000

    Initial Commit

---
+++ slug/slugApp.swift
@@ -0,0 +1,17 @@
+//
+//  slugApp.swift
+//  slug
+//
+//  Created by Winner on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct slugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
